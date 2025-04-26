f007dfb11d7909a74b6354890c3edd24cc99fe17
DeveloperJose <josegperezdev@gmail.com>
2025-04-26 03:43:05 -0600

    Build only dev and prod

--- scripts/config.js
+++ scripts/config.js
@@ -10,7 +10,7 @@ const featureFlags = require('./feature-flags')
 
 const banner =
   '/*!\n' +
-  ` * Vue.js v${version}\n` +
+  ` * (Custom AWBW) Vue.js v${version}\n` +
   ` * (c) 2014-${new Date().getFullYear()} Evan You\n` +
   ' * Released under the MIT License.\n' +
   ' */'
@@ -32,93 +32,12 @@ const consolidatePath = require.resolve('@vue/consolidate/package.json', {
 })
 
 const builds = {
-  // Runtime only (CommonJS). Used by bundlers e.g. Webpack & Browserify
-  'runtime-cjs-dev': {
-    entry: resolve('web/entry-runtime.ts'),
-    dest: resolve('dist/vue.runtime.common.dev.js'),
-    format: 'cjs',
-    env: 'development',
-    banner
-  },
-  'runtime-cjs-prod': {
-    entry: resolve('web/entry-runtime.ts'),
-    dest: resolve('dist/vue.runtime.common.prod.js'),
-    format: 'cjs',
-    env: 'production',
-    banner
-  },
-  // Runtime+compiler CommonJS build (CommonJS)
-  'full-cjs-dev': {
-    entry: resolve('web/entry-runtime-with-compiler.ts'),
-    dest: resolve('dist/vue.common.dev.js'),
-    format: 'cjs',
-    env: 'development',
-    alias: { he: './entity-decoder' },
-    banner
-  },
-  'full-cjs-prod': {
-    entry: resolve('web/entry-runtime-with-compiler.ts'),
-    dest: resolve('dist/vue.common.prod.js'),
-    format: 'cjs',
-    env: 'production',
-    alias: { he: './entity-decoder' },
-    banner
-  },
-  // Runtime only ES modules build (for bundlers)
-  'runtime-esm': {
-    entry: resolve('web/entry-runtime-esm.ts'),
-    dest: resolve('dist/vue.runtime.esm.js'),
-    format: 'es',
-    banner
-  },
-  // Runtime+compiler ES modules build (for bundlers)
-  'full-esm': {
-    entry: resolve('web/entry-runtime-with-compiler-esm.ts'),
-    dest: resolve('dist/vue.esm.js'),
-    format: 'es',
-    alias: { he: './entity-decoder' },
-    banner
-  },
-  // Runtime+compiler ES modules build (for direct import in browser)
-  'full-esm-browser-dev': {
-    entry: resolve('web/entry-runtime-with-compiler-esm.ts'),
-    dest: resolve('dist/vue.esm.browser.js'),
-    format: 'es',
-    transpile: false,
-    env: 'development',
-    alias: { he: './entity-decoder' },
-    banner
-  },
-  // Runtime+compiler ES modules build (for direct import in browser)
-  'full-esm-browser-prod': {
-    entry: resolve('web/entry-runtime-with-compiler-esm.ts'),
-    dest: resolve('dist/vue.esm.browser.min.js'),
-    format: 'es',
-    transpile: false,
-    env: 'production',
-    alias: { he: './entity-decoder' },
-    banner
-  },
-  // runtime-only build (Browser)
-  'runtime-dev': {
-    entry: resolve('web/entry-runtime.ts'),
-    dest: resolve('dist/vue.runtime.js'),
-    format: 'umd',
-    env: 'development',
-    banner
-  },
-  // runtime-only production build (Browser)
-  'runtime-prod': {
-    entry: resolve('web/entry-runtime.ts'),
-    dest: resolve('dist/vue.runtime.min.js'),
-    format: 'umd',
-    env: 'production',
-    banner
-  },
   // Runtime+compiler development build (Browser)
   'full-dev': {
     entry: resolve('web/entry-runtime-with-compiler.ts'),
-    dest: resolve('dist/vue.js'),
+    dest: resolve(
+      '/home/developerjose/local-debian/awbw/public_html/js/vue.js'
+    ),
     format: 'umd',
     env: 'development',
     alias: { he: './entity-decoder' },
@@ -127,101 +46,13 @@ const builds = {
   // Runtime+compiler production build  (Browser)
   'full-prod': {
     entry: resolve('web/entry-runtime-with-compiler.ts'),
-    dest: resolve('dist/vue.min.js'),
+    dest: resolve(
+      '/home/developerjose/local-debian/awbw/public_html/js/vue.min.js'
+    ),
     format: 'umd',
     env: 'production',
     alias: { he: './entity-decoder' },
     banner
-  },
-  // Web compiler (CommonJS).
-  compiler: {
-    entry: resolve('web/entry-compiler.ts'),
-    dest: resolve('packages/template-compiler/build.js'),
-    format: 'cjs',
-    external: Object.keys(
-      require('../packages/template-compiler/package.json').dependencies
-    )
-  },
-  // Web compiler (UMD for in-browser use).
-  'compiler-browser': {
-    entry: resolve('web/entry-compiler.ts'),
-    dest: resolve('packages/template-compiler/browser.js'),
-    format: 'umd',
-    env: 'development',
-    moduleName: 'VueTemplateCompiler',
-    plugins: [node(), cjs()]
-  },
-  // Web server renderer (CommonJS).
-  'server-renderer-dev': {
-    entry: resolve('packages/server-renderer/src/index.ts'),
-    dest: resolve('packages/server-renderer/build.dev.js'),
-    format: 'cjs',
-    env: 'development',
-    external: [
-      'stream',
-      ...Object.keys(
-        require('../packages/server-renderer/package.json').dependencies
-      )
-    ]
-  },
-  'server-renderer-prod': {
-    entry: resolve('server/index.ts'),
-    dest: resolve('packages/server-renderer/build.prod.js'),
-    format: 'cjs',
-    env: 'production',
-    external: [
-      'stream',
-      ...Object.keys(
-        require('../packages/server-renderer/package.json').dependencies
-      )
-    ]
-  },
-  'server-renderer-basic': {
-    entry: resolve('server/index-basic.ts'),
-    dest: resolve('packages/server-renderer/basic.js'),
-    format: 'umd',
-    env: 'development',
-    moduleName: 'renderVueComponentToString',
-    plugins: [node(), cjs()]
-  },
-  'server-renderer-webpack-server-plugin': {
-    entry: resolve('server/webpack-plugin/server.ts'),
-    dest: resolve('packages/server-renderer/server-plugin.js'),
-    format: 'cjs',
-    external: Object.keys(
-      require('../packages/server-renderer/package.json').dependencies
-    )
-  },
-  'server-renderer-webpack-client-plugin': {
-    entry: resolve('server/webpack-plugin/client.ts'),
-    dest: resolve('packages/server-renderer/client-plugin.js'),
-    format: 'cjs',
-    external: Object.keys(
-      require('../packages/server-renderer/package.json').dependencies
-    )
-  },
-  'compiler-sfc': {
-    entry: resolve('packages/compiler-sfc/src/index.ts'),
-    dest: resolve('packages/compiler-sfc/dist/compiler-sfc.js'),
-    format: 'cjs',
-    external: Object.keys(
-      require('../packages/compiler-sfc/package.json').dependencies
-    ),
-    plugins: [
-      node({ preferBuiltins: true }),
-      cjs({
-        ignore: [
-          ...Object.keys(require(consolidatePath).devDependencies),
-          'vm',
-          'crypto',
-          'react-dom/server',
-          'teacup/lib/express',
-          'arc-templates/dist/es5',
-          'then-pug',
-          'then-jade'
-        ]
-      })
-    ]
   }
 }
 
